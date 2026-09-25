06989f4b91e3906344df8b4dcd0dd18b2c415d9a
Arya <aryaabhishek200059@gmail.com>
2026-09-25 15:35:02 +0530

    build and validate flat mart

--- 2_DW_Mart_Build/02_load_schema_dw.sql
+++ 2_DW_Mart_Build/02_load_schema_dw.sql
@@ -1,4 +1,4 @@
--- duckdb dw_marts.duckdb -c ".read build_dw_marts.sql"
+
 
 select '=== Loading company_dim Table ===' as info;
 
--- 2_DW_Mart_Build/build_marts.sql
+++ 2_DW_Mart_Build/build_marts.sql
@@ -1,9 +1,13 @@
+-- duckdb dw_marts.duckdb -c ".read build_marts.sql"
 
 -- step 1: dw - create star schema
 .read 01_create_tables_dw.sql
 
 -- step 2: dw - load data from csv files into tables
 .read 02_load_schema_dw.sql 
+
+-- step 3: mart - create flat mart
+.read 03_create_flat_mart.sql 
 
 
 SHOW TABLES;
