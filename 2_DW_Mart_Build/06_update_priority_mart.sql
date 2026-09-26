91378fb0e5a95c94fe2d24ffa0f6305d018150e8
Arya <aryaabhishek200059@gmail.com>
2026-09-26 10:46:50 +0530

    build priority mart and update mart

---
+++ 2_DW_Mart_Build/06_update_priority_mart.sql
@@ -0,0 +1,84 @@
+-- step 6: mart - update priority roles mart
+
+select '=== updating roles for priority mart ===' as info; 
+
+-- update data engineer to priority 1
+update priority_mart.priority_roles
+set priority_lvl = 1
+where role_name = 'Data Engineer';
+
+-- add data scientist as level 3
+insert into priority_mart.priority_roles (
+    role_id,
+    role_name,
+    priority_lvl
+)
+values
+    (4, 'Data Scientist', 3);
+
+select * from priority_mart.priority_roles;
+
+select '=== creating temp source table for priority mart ===' as info; 
+
+-- create temp table
+create or replace temp table src_priority_jobs as
+select 
+    jpf.job_id,
+    jpf.job_title_short,
+    cd.name as company_name,
+    jpf.job_posted_date,
+    jpf.salary_year_avg,
+    r.priority_lvl,
+    current_timestamp as updated_at
+from
+    job_postings_fact as jpf
+left join 
+    company_dim as cd
+    on jpf.company_id = cd.company_id
+inner join priority_mart.priority_roles as r 
+    on jpf.job_title_short  = r.role_name
+;
+
+select '=== batch updating priority_jobs_snapshot for priority mart ===' as info; 
+
+-- merge into statement
+merge into priority_mart.priority_jobs_snapshot as tgt
+using src_priority_jobs as src
+on tgt.job_id = src.job_id
+
+when matched and tgt.priority_lvl is distinct from src.priority_lvl then
+    update set
+        priority_lvl = src.priority_lvl,
+        updated_at = src.updated_at
+
+when not matched then
+    insert (
+            job_id,
+            job_title_short,
+            company_name,
+            job_posted_date,
+            salary_year_avg,
+            priority_lvl,
+            updated_at
+        )
+        values (
+            src.job_id,
+            src.job_title_short,
+            src.company_name,
+            src.job_posted_date,
+            src.salary_year_avg,
+            src.priority_lvl,
+            src.updated_at)
+
+when not matched by source then delete;
+
+
+-- final check querry
+select 
+    job_title_short,
+    count(*) as job_count,
+    min(priority_lvl) as priority_lvl,
+    min(updated_at) as updated_at
+from priority_mart.priority_jobs_snapshot
+group by job_title_short
+order by job_count desc;
